035e13e03984e97256cc07b93f785765d175d0d3
z2huo <z2huo9994@163.com>
2025-12-16 22:43:57 +0800

    fix

--- sentinel-dashboard/src/main/java/com/alibaba/csp/sentinel/dashboard/rule/nacos/system/SystemRuleNacosPublisher.java
+++ sentinel-dashboard/src/main/java/com/alibaba/csp/sentinel/dashboard/rule/nacos/system/SystemRuleNacosPublisher.java
@@ -32,7 +32,7 @@ public class SystemRuleNacosPublisher implements DynamicRulePublisher<List<Syste
         if (rules == null) {
             return;
         }
-        configService.publishConfig(app + NacosConfigUtil.AUTHORITY_DATA_ID_POSTFIX,
+        configService.publishConfig(app + NacosConfigUtil.SYSTEM_DATA_ID_POSTFIX,
                 NacosConfigUtil.GROUP_ID, converter.convert(rules), ConfigType.JSON.getType());
     }
 }
